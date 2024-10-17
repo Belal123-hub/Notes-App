a74dbd25ca0a562ca77214aa2bb68c2457047c05
Belal123-hub <131976726+Belal123-hub@users.noreply.github.com>
2024-10-17 22:57:01 +0700

    creating and implementing NotesViewModel and Adding AddNoteUseCase to usecases

--- app/src/main/java/com/example/notesapp/di/AppModule.kt
+++ app/src/main/java/com/example/notesapp/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.example.notesapp.feature_note.data.data_source.NoteDatabase
 import com.example.notesapp.feature_note.data.repository.NoteRepositoryImpl
 import com.example.notesapp.feature_note.domain.repository.NoteRepository
+import com.example.notesapp.feature_note.domain.use_case.AddNoteUseCase
 import com.example.notesapp.feature_note.domain.use_case.DeleteNoteUseCase
 import com.example.notesapp.feature_note.domain.use_case.GetNotesUseCase
 import com.example.notesapp.feature_note.domain.use_case.NoteUseCases
@@ -40,7 +41,8 @@ class AppModule {
     fun provideNoteUseCases(repository: NoteRepository): NoteUseCases{
         return NoteUseCases(
             getNotesUseCase = GetNotesUseCase(repository),
-            deleteNoteUseCase = DeleteNoteUseCase(repository)
+            deleteNoteUseCase = DeleteNoteUseCase(repository),
+            addNoteUseCase = AddNoteUseCase(repository)
         )
     }
 }
