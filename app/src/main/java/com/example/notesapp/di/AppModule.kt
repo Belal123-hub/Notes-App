2aec1b831699c786b3d8c4593f4294482db3285d
Belal123-hub <131976726+Belal123-hub@users.noreply.github.com>
2024-10-18 19:22:32 +0700

    Preparing  ViewModel for AddEditScreen with NoteTextFieldState and AddEditNoteEvent

--- app/src/main/java/com/example/notesapp/di/AppModule.kt
+++ app/src/main/java/com/example/notesapp/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.example.notesapp.feature_note.data.repository.NoteRepositoryImpl
 import com.example.notesapp.feature_note.domain.repository.NoteRepository
 import com.example.notesapp.feature_note.domain.use_case.AddNoteUseCase
 import com.example.notesapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.example.notesapp.feature_note.domain.use_case.GetNoteUseCase
 import com.example.notesapp.feature_note.domain.use_case.GetNotesUseCase
 import com.example.notesapp.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -42,7 +43,8 @@ class AppModule {
         return NoteUseCases(
             getNotesUseCase = GetNotesUseCase(repository),
             deleteNoteUseCase = DeleteNoteUseCase(repository),
-            addNoteUseCase = AddNoteUseCase(repository)
+            addNoteUseCase = AddNoteUseCase(repository),
+            getNoteUseCase = GetNoteUseCase(repository)
         )
     }
 }
